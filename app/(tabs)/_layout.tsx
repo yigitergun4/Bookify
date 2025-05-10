941d5282818cd18da7357d8a76d1c94172c3647a
yigitergun <yigitergunn4@gmail.com>
2025-05-10 15:18:09 +0300

    asdasd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,106 +1,104 @@
-import React from 'react';
-import {Image, Text, View} from "react-native";
-import { Tabs } from 'expo-router';
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import homeIcon from '@/assets/images/homeicon.png'
-import {DarkTheme} from "@react-navigation/native/src";
+import { Image, Text, View } from "react-native";
+import { Tabs } from "expo-router";
+import homeIcon from "@/assets/images/homeicon.png";
 
 const HomeIcon = () => (
-    <Image src={homeIcon} style={{ width: 40, height: 40 }} resizeMode="contain" />
+  <Image
+    src={homeIcon}
+    style={{ width: 40, height: 40 }}
+    resizeMode="contain"
+  />
 );
 
 type TabsViewProps = {
-    color: string;
-    size: number;
-    label: string;
-    source: any;
+  color: string;
+  size: number;
+  label: string;
+  source: any;
 };
 
 const TabsView = ({ color, size, label, source }: TabsViewProps) => {
-    return (
-        <View style={{ alignItems: 'center', minWidth: 60 }}>
-            <Image
-                source={source}
-                style={{
-                    width: size,
-                    height: size,
-                    marginBottom: 5,
-                }}
-                resizeMode="contain"
-            />
-            <Text style={{ color, fontSize: 12 }}>{label}</Text>
-        </View>
-    );
+  return (
+    <View style={{ alignItems: "center", minWidth: 60 }}>
+      <Image
+        source={source}
+        style={{
+          width: size,
+          height: size,
+          marginBottom: 5,
+        }}
+        resizeMode="contain"
+      />
+      <Text style={{ color, fontSize: 12 }}>{label}</Text>
+    </View>
+  );
 };
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
-
-    return (
-        <Tabs
-            screenOptions={{
-                tabBarStyle: {
-                    paddingTop: 10,
-                    backgroundColor: colorScheme === 'dark' ? "white" : "dark",
-                },
-                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-                // Disable the static render of the header on web
-                // to prevent a hydration error in React Navigation v6.
-                headerShown:false,
-            }}>
-            <Tabs.Screen
-                name="homefolder"
-                options={{
-                    tabBarShowLabel: false,
-                    tabBarIcon: ({ size }:any) => (
-                        <TabsView
-                            color="black"
-                            size={size}
-                            label="Home"
-                            source={require('@/assets/images/homeicon.png')}
-                        />),
-                }}
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          paddingTop: 10,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="homefolder"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size }: any) => (
+            <TabsView
+              color="black"
+              size={size}
+              label="Home"
+              source={require("@/assets/images/homeicon.png")}
             />
-            <Tabs.Screen
-                name="search/index"
-                options={{
-                    tabBarShowLabel: false,
-                    tabBarIcon: ({ size }:any) => (
-                        <TabsView
-                            color="black"
-                            size={size}
-                            label="Search"
-                            source={require('@/assets/images/searchicon.png')}
-                        />),
-                }}
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size }: any) => (
+            <TabsView
+              color="black"
+              size={size}
+              label="Search"
+              source={require("@/assets/images/searchicon.png")}
             />
-            <Tabs.Screen
-                name="reading/index"
-                options={{
-                    tabBarShowLabel: false,
-                    tabBarIcon: ({ size }:any) => (
-                        <TabsView
-                            color="black"
-                            size={size}
-                            label="Reading"
-                            source={require('@/assets/images/readingicon.png')}
-                        />),
-                }}
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="reading/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size }: any) => (
+            <TabsView
+              color="black"
+              size={size}
+              label="Reading"
+              source={require("@/assets/images/readingicon.png")}
             />
-            <Tabs.Screen
-                name="profile/index"
-                options={{
-                    tabBarShowLabel: false,
-                    tabBarIcon: ({ size }:any) => (
-                        <TabsView
-                            color="black"
-                            size={size}
-                            label="My Profile"
-                            source={require("@/assets/images/profileicon.png")}
-                        />),
-                }}
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size }: any) => (
+            <TabsView
+              color="black"
+              size={size}
+              label="My Profile"
+              source={require("@/assets/images/profileicon.png")}
             />
-        </Tabs>
-    );
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
